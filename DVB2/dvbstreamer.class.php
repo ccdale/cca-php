b0ef523103cb842ae5ef6d311318b2ab7a270418
Chris Allison <chris.allison@hotmail.com>
2014-06-01 20:04:15 +0100

    initial draft of dvbstreamer class for dvb2

--- DVB2/dvbstreamer.class.php
+++ DVB2/dvbstreamer.class.php
@@ -10,11 +10,10 @@
  * chris.allison@hotmail.com
  *
  * Started: Sunday  1 June 2014, 08:50:32
- * Last Modified: Sunday  1 June 2014, 09:33:11
+ * Last Modified: Sunday  1 June 2014, 19:55:45
  */
 
 require_once "base.class.php";
-require_once "Unix/process.class.php";
 
 class DvbStreamer extends Base
 {
@@ -25,6 +24,7 @@ class DvbStreamer extends Base
     private $pidfile;
     private $user;
     private $pass;
+    private $dontstop;
 
     public function __construct($logg=false,$adaptor=0,$user="tvc",$pass="tvc")/*{{{*/
     {
@@ -38,21 +38,59 @@ class DvbStreamer extends Base
         $this->cmd="/usr/bin/dvbstreamer -a " . $this->adaptor . " -u " . $this->user . " -p " . $this->pass . " -d";
         if($this->isRunning()){
             $this->info("dvbstreamer for adaptor " . $this->adaptor . " is already running");
+            $this->dontstop=true;
         }else{
             $this->info("dvbstreamer for adaptor " . $this->adaptor . " is not running.");
+            $this->dontstop=false;
         }
+        $this->startDvbstreamer();
     }/*}}}*/
     public function __destruct()/*{{{*/
     {
+        $this->stopDvbstreamer();
         parent::__destruct();
+    }/*}}}*/
+    public function stopDvbstreamer()/*{{{*/
+    {
+        if(false==$this->dontstop){
+            if(file_exists($this->pidfile)){
+                $cpid=file_get_contents($this->pidfile);
+                $procdir="/proc/$cpid";
+                if(file_exists($procdir) && is_dir($procdir)){
+                    $this->info("Stopping dvbstreamer for adaptor: " . $this->adaptor);
+                    if(false===($junk=posix_kill($cpid,SIGTERM))){
+                        $str=posix_strerror(posix_get_last_error());
+                        $this->error("error killing dvbstreamer for adaptor: " . $this->adaptor);
+                        $this->error("Posix says: $str");
+                    }
+                }
+            }
+        }else{
+            $this->info("not attempting to stop dvbstreamer for adaptor: " . $this->adaptor . " as we didn't start it");
+        }
     }/*}}}*/
     public function startDvbstreamer()/*{{{*/
     {
-        $pidfile=$this->unixPath($this->dvbsdir) . "dvbstreamer-$adaptor.pid";
-        if(file_exists($pidfile)){
-            $cpid=file_get_contents($pidfile);
-            $proc=new Process($cpid);
-            $pcmd=$proc->getCmdline();
+        if($this->dontstop){
+            $this->debug("another process is already running for adaptor: " . $this->adaptor . " not starting another one.");
+            return true;
+        }else{
+            $this->debug("starting process for adaptor: " . $this->adaptor);
+            $lastline=exec($this->cmd,$op,$ecode);
+            $this->debug("Checking to see if process started ok");
+            // we may need to sleep a bit here to
+            // allow dvbstreamer to write it't pid file
+            // give it 10 seconds
+            for($iter=0;$iter<10;$iter++){
+                if($this->isRunning()){
+                    $this->info("dvbstreamer started ok for adaptor: " . $this->adaptor);
+                    return true;
+                }
+                $this->debug("waiting for dvbstreamer to start");
+                sleep(1);
+            }
+            $this->error("failed to start dvbstreamer for adaptor: " . $this->adaptor);
+            return false;
         }
     }/*}}}*/
     private function isRunning()/*{{{*/
@@ -61,16 +99,12 @@ class DvbStreamer extends Base
         if(file_exists($this->pidfile)){
             $this->debug("pidfile exists");
             $cpid=file_get_contents($this->pidfile);
-            if(false!=($junk=$proc=new Process($cpid))){
-                $this->debug("pid process exists");
-                $pcmd=$proc->getCmdline();
-                $this->debug("process cmdline: $pcmd");
-                if($pcmd==$this->cmd){
-                    $ret=true;
-                    $this->debug("this process is already running");
-                }else{
-                    $this->debug("This isn't our process");
-                }
+            $pdir="/proc/$cpid";
+            if(file_exists($cpid) && is_dir($cpid)){
+                $this->debug("process from pidfile exists");
+                $ret=true;
+            }else{
+                $this->debug("process from pidfile does not exist");
             }
         }
         return $ret;
